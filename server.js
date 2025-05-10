2f0e381e665c501f96779fb862d673e615f9f9e3
Nada-omri <118102324+Nada-omri@users.noreply.github.com>
2025-05-10 20:09:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
+const express = require('express'); // You missed this line
 const mysql = require('mysql');
 
+const app = express();
+app.use(express.json());
+
 const db = mysql.createConnection({
-
-  host: 'terraform-20250508213528251100000001.ctsoiw8yyjzh.eu-west-3.rds.amazonaws.com', // Use RDS endpoint if using Amazon RDS
-
+  host: 'terraform-20250508213528251100000001.ctsoiw8yyjzh.eu-west-3.rds.amazonaws.com', // RDS endpoint
   user: 'admin',
-
   password: 'mypassword123',
-
   database: 'two_tier_db1'
-
 });
 
 db.connect((err) => {
-
-  if (err) throw err;
-
+  if (err) {
+    console.error('Error connecting to MySQL:', err.message);
+    return;
+  }
   console.log('Connected to MySQL database');
-
 });
 
-app.get('/data', (req, res) => {
-
-  db.query('SELECT * FROM your_table', (err, results) => {
-
-    if (err) throw err;
-
-    res.send(results);
-
+app.get('/todos', (req, res) => {
+  db.query('SELECT * FROM todos', (err, results) => {
+    if (err) {
+      console.error('Query error:', err.message);
+      return res.status(500).send('Database query failed');
+    }
+    res.json(results);
   });
-
 });
+
+
