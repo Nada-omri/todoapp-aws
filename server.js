6df99aad9efc580b17c4e890af24e93fc5648f91
Nada-omri <118102324+Nada-omri@users.noreply.github.com>
2025-05-10 20:18:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.get('/todos', (req, res) => {
     res.json(results);
   });
 });
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4001;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
